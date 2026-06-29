bbc8bb4b5a1a338fd0247d55f83a62b4c14055d4
KANAPARTHI SAI DHEERAJ <139680709+SaiDheeraj-72@users.noreply.github.com>
2026-06-29 12:24:03 +0530

    Create object_types.py

---
+++ Module-05-Python-Bash/Python-Scripts/object_types.py
@@ -0,0 +1,44 @@
+# Lists:
+
+Code
+fruits = ["Apple", "Banana", "Orange"]
+
+print(fruits)
+print(fruits[1])
+
+#Output
+['Apple', 'Banana', 'Orange']
+Banana
+Explanation
+
+Lists are ordered and mutable collections.
+
+# Tuples
+Code
+numbers = (10,20,30)
+
+print(numbers)
+
+# Output
+(10, 20, 30)
+Explanation
+
+Tuples are ordered but immutable collections.
+
+# Dictionary
+Code
+student = {
+    "Name":"Sai",
+    "Age":21,
+    "Branch":"ECE"
+}
+
+print(student)
+print(student["Name"])
+
+# Output
+{'Name': 'Sai', 'Age': 21, 'Branch': 'ECE'}
+Sai
+Explanation
+
+Dictionaries store data as key-value pairs.
