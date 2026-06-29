2d5bedc7a40ba212b6b4c9cb5d550f79a8624b55
KANAPARTHI SAI DHEERAJ <139680709+SaiDheeraj-72@users.noreply.github.com>
2026-06-29 12:12:10 +0530

    Create operators.py

---
+++ Module-05-Python-Bash/Python-Scripts/operators.py
@@ -0,0 +1,16 @@
+a = 20
+b = 5
+
+print("Addition :", a+b)
+print("Subtraction :", a-b)
+print("Multiplication :", a*b)
+print("Division :", a/b)
+print("Modulus :", a%b)
+
+
+Output
+Addition : 25
+Subtraction : 15
+Multiplication : 100
+Division : 4.0
+Modulus : 0
