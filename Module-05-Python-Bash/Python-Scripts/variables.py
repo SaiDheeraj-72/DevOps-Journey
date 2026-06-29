22ef22890db98603991328f90a9825fd287223c1
KANAPARTHI SAI DHEERAJ <139680709+SaiDheeraj-72@users.noreply.github.com>
2026-06-29 12:11:05 +0530

    Create variables.py

---
+++ Module-05-Python-Bash/Python-Scripts/variables.py
@@ -0,0 +1,14 @@
+# Variables Example
+
+name = "Sai Dheeraj"
+age = 21
+cgpa = 9.11
+
+print("Name :", name)
+print("Age :", age)
+print("CGPA :", cgpa)
+
+Output
+Name : Sai Dheeraj
+Age : 21
+CGPA : 9.11
