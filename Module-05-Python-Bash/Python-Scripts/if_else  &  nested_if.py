8b23b62bbef7dd63a1ddfcdfa8d3f035349fd512
KANAPARTHI SAI DHEERAJ <139680709+SaiDheeraj-72@users.noreply.github.com>
2026-06-29 12:14:02 +0530

    Create if_else  &  nested_if.py

---
+++ Module-05-Python-Bash/Python-Scripts/if_else  &  nested_if.py
@@ -0,0 +1,36 @@
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+
+elif marks >= 80:
+    print("Grade B")
+
+elif marks >= 70:
+    print("Grade C")
+
+else:
+    print("Fail")
+
+
+Output
+Grade B
+
+
+
+//nested-if
+  
+  age = 22
+citizen = True
+
+if age >= 18:
+    if citizen:
+        print("Eligible to Vote")
+    else:
+        print("Not a Citizen")
+else:
+    print("Not Eligible")
+
+
+Output
+Eligible to Vote
